71722e1c5f00c11da48e83177e78f6451ea2ddf9
Hasan Zahra <zahrafamily215@gmail.com>
2021-03-27 15:32:32 -0500

    deleted:    commit.sh 	modified:   css/index.css 	modified:   index.html 	deleted:    index.js 	new file:   media/no-facebook.png 	modified:   other/index.js

--- other/index.js
+++ other/index.js
@@ -9,6 +9,7 @@ var html_head =
     <link rel="stylesheet" href="/article.css">
     <title>
 `;
+var darkMode = false;
 
 function make_article(title, author, date, text) {
     var final_text = html_head + title.trim() + " - Hasan Zahra's Website</title>\n";
@@ -23,3 +24,33 @@ function make_article(title, author, date, text) {
     document.body.innerHTML = final_text;
 }
 
+
+function dark_mode() {
+    if (!darkMode) {
+        document.body.style = `
+        background-color: #000000;
+        color: #b9b4b4;
+        `;
+
+        var x = document.querySelectorAll("code");
+        var i;
+        for (i = 0; i < x.length; i++)
+            x[i].style.backgroundColor = "#a9a9a94b";
+        
+        document.getElementById('darkmode-btn').innerText = "Light Mode"
+        darkMode = true;
+    } else {
+        document.body.style = `
+        background-color: #f6f2f2;
+        color: #444;
+        `;
+
+        var x = document.querySelectorAll("code");
+        var i;
+        for (i = 0; i < x.length; i++)
+            x[i].style.backgroundColor = "#d6d2d2";
+
+        document.getElementById('darkmode-btn').innerText = "Dark Mode"
+        darkMode = false;
+    }
+}
